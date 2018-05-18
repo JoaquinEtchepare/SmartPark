28e03bcc782b9dc7e3fe8fec2119f94259a54f03
JoaquinEtchepare <39206129+JoaquinEtchepare@users.noreply.github.com>
2018-05-17 22:34:33 -0300

    primeros calculos

--- App/smartpark.js
+++ App/smartpark.js
@@ -1,21 +1,71 @@
+var estacionamiento=[];
+function crearEstacionamiento(filas,columnas){
+  var lugares=parseInt(filas)*parseInt(columnas);
 
-      var lectorqr = new Instascan.Scanner({ video: document.getElementById('monitor') });
-      lectorqr.addListener('scan', function (contenido) {
-      var smartCard = JSON.parse(contenido)
-
-        swal(
-  'Exito!',
-  'Se detecto el vehiculo ' +smartCard.d+", matricula: "+smartCard.m,
-  'success'
-      );
-
-      });
-      Instascan.Camera.getCameras().then(function (cameras) {
-        if (cameras.length > 0) {
-          lectorqr.start(cameras[0]);
-        } else {
-          console.error('No hay camaras.');
+  for (var i = 0; i < lugares; i++) {
+    var lugarVacio={
+      ocupado: false,
+      nombre: null,
+      ci: null,
+      matricula: null,
+      detalle: null,
+      ingreso: null,
+    }
+    estacionamiento[i]=lugarVacio;
+  }
+}
+  function buscarLugar(){
+    var lugar=null;
+    estacionamiento.forEach(
+      function(item,index) {
+        if (lugar==null && item.ocupado==false) {
+          lugar=index;
         }
-      }).catch(function (e) {
-        console.error(e);
-      });
+      }
+      );
+    return lugar;
+  }
+  function ocuparLugar(nroLugar,cliente){
+    var lugar={
+      ocupado: true,
+      nombre: cliente.nombre,
+      ci: cliente.ci,
+      matricula: cliente.matricula,
+      detalle: cliente.detalle,
+      ingreso: Date.now(),
+    }
+    estacionamiento[nroLugar]=lugar;
+
+  }
+  function liberarLugar(nroLugar){
+    var lugarVacio={
+      ocupado: false,
+      nombre: null,
+      ci: null,
+      matricula: null,
+      detalle: null,
+      ingreso: null,
+    }
+    estacionamiento[nroLugar]=lugarVacio;
+  }
+
+var lectorqr = new Instascan.Scanner({ video: document.getElementById('monitor') });
+lectorqr.addListener('scan', function (contenido) {
+  var smartCard = JSON.parse(contenido)
+
+  swal(
+    'Exito!',
+    'Se detecto el vehiculo ' +smartCard.d+", matricula: "+smartCard.m,
+    'success'
+    );
+
+});
+Instascan.Camera.getCameras().then(function (cameras) {
+  if (cameras.length > 0) {
+    lectorqr.start(cameras[0]);
+  } else {
+    console.error('No hay camaras.');
+  }
+}).catch(function (e) {
+  console.error(e);
+});
