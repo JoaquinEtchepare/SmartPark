0cdb11d739a7e20a471f9edd2c0eedeeca365d70
Joaquin Peraza <joaquin@peraza.uy>
2018-06-05 13:18:51 -0300

    auto de mario!!!!
    agregue la grafica de torata

--- App/smartpark-admin.js
+++ App/smartpark-admin.js
@@ -1,29 +1,30 @@
 ///admin
 var a, b, c, d, e;
 document.addEventListener("keypress", unlock);
-var porcentajes=[];
+var porcentajes = [];
 var table = document.getElementById("lista");
-function agregarfila(n, nombre, matricula, detalle, entrada, salida){
- // Create an empty <tr> element and add it to the 1st position of the table:
-var row = table.insertRow(0);
 
-// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-var cell1 = row.insertCell(0);
-var cell2 = row.insertCell(1);
-var cell3 = row.insertCell(2);
-var cell4 = row.insertCell(3);
-var cell5 = row.insertCell(4);
-var cell6 = row.insertCell(5);
+function agregarfila(n, nombre, matricula, detalle, entrada, salida) {
+    // Create an empty <tr> element and add it to the 1st position of the table:
+    var row = table.insertRow(0);
 
-// Add some text to the new cells:
-cell1.innerHTML = n;
-cell2.innerHTML = nombre;
-cell3.innerHTML = matricula;
-cell4.innerHTML = detalle;
-cell5.innerHTML = moment(entrada).format("D, MMM YYYY, h:mm a");
-if (salida>0) {
-    cell6.innerHTML = moment(salida*1000).format("D, MMM YYYY, h:mm a");
-}
+    // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    var cell6 = row.insertCell(5);
+
+    // Add some text to the new cells:
+    cell1.innerHTML = n;
+    cell2.innerHTML = nombre;
+    cell3.innerHTML = matricula;
+    cell4.innerHTML = detalle;
+    cell5.innerHTML = moment(entrada).format("D, MMM YYYY, h:mm a");
+    if (salida > 0) {
+        cell6.innerHTML = moment(salida * 1000).format("D, MMM YYYY, h:mm a");
+    }
 }
 
 function unlock(evento) {
@@ -47,7 +48,7 @@ function iniciarAdmin() {
             .attr("id", "bg" + i) // adds the id
             .addClass("btn-group");
         $("#mapa").append(buttonGroup);
-         $("#mapa").append("<br>");
+        $("#mapa").append("<br>");
 
         for (var i2 = 0; i2 < ajustes.columnas; i2++) {
             var casillero = ('0' + iLugar).slice(-2);
@@ -64,12 +65,12 @@ function iniciarAdmin() {
                 desc = estacionamiento[iLugar - 1].nombre + '<br>' + estacionamiento[iLugar - 1].matricula + '<br>' + estacionamiento[iLugar - 1].detalle;
             } else {
                 clase += "success";
-            
+
             };
 
-                if (estacionamiento[iLugar - 1].ocupado==true) {
-                    var Lugar= estacionamiento[iLugar - 1];
-                    agregarfila(iLugar, Lugar.nombre, Lugar.matricula, Lugar.detalle, Lugar.ingreso, Lugar.salida);
+            if (estacionamiento[iLugar - 1].ocupado == true) {
+                var Lugar = estacionamiento[iLugar - 1];
+                agregarfila(iLugar, Lugar.nombre, Lugar.matricula, Lugar.detalle, Lugar.ingreso, Lugar.salida);
             }
             var boton = $('<button/>').attr({
                 name: 'btn' + i2,
@@ -87,30 +88,31 @@ function iniciarAdmin() {
     graficar();
 
 }
-function guardarPorcentaje(){
-    var ocupados=0; 
-    var vacios=0;
-    estacionamiento.forEach(
-            function(item){
-                if (item.ocupado==true) {
-                    ocupados= ocupados+1;
-                } else{
-                    vacios=vacios+1;
-                }
 
+function guardarPorcentaje() {
+    var ocupados = 0;
+    var vacios = 0;
+    estacionamiento.forEach(
+        function (item) {
+            if (item.ocupado == true) {
+                ocupados = ocupados + 1;
+            } else {
+                vacios = vacios + 1;
             }
-        );
-    var total= ocupados+vacios;
-    var porcentaje= ocupados/total*100;
-    var hora= Date.now();
-    var temporal={
+
+        }
+    );
+    var total = ocupados + vacios;
+    var porcentaje = ocupados / total * 100;
+    var hora = Date.now();
+    var temporal = {
         porcentaje: porcentaje,
         horas: hora
-        }
-        porcentajes.push(temporal);
+    }
+    porcentajes.push(temporal);
 
 }
-setInterval(guardarPorcentaje, ajustes.frecuencia*60*60*1000);
+setInterval(guardarPorcentaje, ajustes.frecuencia * 60 * 60 * 1000);
 guardarPorcentaje();
 
 
@@ -118,50 +120,62 @@ guardarPorcentaje();
 
 
 
-function graficar(){
+function graficar() {
     var ctx = document.getElementById("myChart");
-    var valores=[];
-    var horas=[];
+    var valores = [];
+    var horas = [];
     porcentajes.forEach(
-            function(item){
-             valores.push(item.porcentaje);
+        function (item) {
+            valores.push(item.porcentaje);
 
-             horas.push(moment(item.horas).format("D, MMM YYYY, h:mm a"));
-             
-}
-);
+            horas.push(moment(item.horas).format("D, MMM YYYY, h:mm a"));
 
-            
-        
-    var scatterChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        datasets: [{
-            label: "% Lugares ocupados",
-            backgroundColor:"rgba(150,0,0,0.5)",
-        borderColor:"rgba(150,0,0,0.3)",
-       
-            data: valores
-        }]
-    },
-    options: {
-        
-        scales: {
-            xAxes: [{
-                type: 'category',
-                labels: horas
-                
-            }],
-            yAxes: [{
-                 ticks: {
-                    beginAtZero:true,
-                     min: 0,
-                max: 100
-                }
-               
-            }]
         }
-    }
-});
+    );
+
+
+
+    var scatterChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            datasets: [{
+                label: "% Lugares ocupados",
+                backgroundColor: "rgba(150,0,0,0.5)",
+                borderColor: "rgba(150,0,0,0.3)",
+
+                data: valores
+        }]
+        },
+        options: {
+
+            scales: {
+                xAxes: [{
+                    type: 'category',
+                    labels: horas
+
+            }],
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        min: 0,
+                        max: 100
+                    }
+
+            }]
+            }
+        }
+    });
+    var ctx2 = document.getElementById("myChart2");
+    var myDoughnutChart = new Chart(ctx2, {
+        type: 'doughnut',
+        data: {
+            datasets: [{
+             data:   [alcancia.horario, alcancia.diario, alcancia.recargo],
+                backgroundColor: ["#0074D9", "#10b020", "#FF4136"]
+            }],
+            labels: ['Estadias Horarias', 'Estadias Diarias', 'Recargos']
+        }
+    });
+
 
 }
